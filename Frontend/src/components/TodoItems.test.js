493d49f72ba6dfecf76eeabcea3fd8fc97f956df
dileep_samala <dileep_samala@yahoo.com>
2021-12-06 12:38:17 +1300

    Updated to remove act() error in test

--- Frontend/src/components/TodoItems.test.js
+++ Frontend/src/components/TodoItems.test.js
@@ -1,4 +1,4 @@
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, waitFor } from '@testing-library/react'
 import TodoItems from './TodoItems'
 
 jest.mock('../services/TodoItemsService')
@@ -6,11 +6,10 @@ jest.mock('../services/TodoItemsService')
 describe('todo items tests', () => {
   afterEach(cleanup)
 
-  it('should render a table', (done) => {
+  it('should render a table', async () => {
     const { container } = render(<TodoItems addedItem={undefined} />)
     const table = container.getElementsByTagName('table')
-    done()
     expect(table).toHaveLength(1)
-    expect(container).not.toBeNull()
+    await waitFor(() => { expect(container).not.toBeNull() })
   })
 })
